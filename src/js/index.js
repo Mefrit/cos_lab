9aeafb21e713508f6ed565cbb03325f6a87f8499
Mefrit <mihail_fokin_1999@mail.ru>
2021-04-07 03:40:51 +0300

    add style

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 "use strict";
 class CanvasImage {
-    constructor(canvas, src) {
+    constructor(canvas, canvas_init, src, size_matrix) {
         // загрузка изображения на холст
-        var context = canvas.getContext('2d');
+        var context = canvas.getContext('2d'), context_init = canvas_init.getContext('2d');
         var i = new Image();
         var that = this;
         i.onload = function () {
             canvas.width = i.width;
             canvas.height = i.height;
+            canvas_init.width = i.width;
+            canvas_init.height = i.height;
+            context_init.drawImage(i, 0, 0, i.width, i.height);
             context.drawImage(i, 0, 0, i.width, i.height);
             // запомним оригинальные пикселы
             console.log(that);
             that.original = that.getData();
         };
         i.src = src;
+        this.size_matrix = size_matrix;
         // кешируем
         this.context = context;
         this.image = i;
@@ -35,8 +39,7 @@ class CanvasImage {
         var oldpx = olddata.data;
         var newdata = this.context.createImageData(olddata);
         var newpx = newdata.data
-        var res = [];
-        var len = newpx.length;
+
         // this.median(newdata, 3, 3);
         // for (var i = 0; i < len; i += 4) {
         //     res = fn.call(this, oldpx[i], oldpx[i + 1], oldpx[i + 2], oldpx[i + 3], factor, i);
@@ -45,22 +48,23 @@ class CanvasImage {
         //     newpx[i + 2] = res[2]; // b
         //     newpx[i + 3] = res[3]; // a
         // }
-        console.log(this.median(olddata, 9, 3));
-        this.setData(this.median(olddata, 3, 3));
+        // console.log(this.median(olddata, 9, 3));
+        console.log("this.size_matrix", this.size_matrix);
+        this.setData(this.median(olddata, this.size_matrix, 3));
     };
     median(pixels, size, colors) {
         var d = pixels.data;
         var buff = new pixels.data.constructor(new ArrayBuffer(pixels.data.length));
         var w = pixels.width;
         var h = pixels.height;
-        const bpr = w * 4; // Bytes per row
+        const bpr = w * 4; // Байты на строку
 
         if (size < 3) return;
 
         const ms = Math.floor(size / 2);
         const mc = Math.floor((size * size) / 2);
 
-        // Find the middle value by histogram traversal
+        // Найти среднее значение путем обхода гистограммы
         function getMiddle(h, n) {
             for (var count = h[0], i = 1; i < 256; i++, count += h[i]) {
                 if (count >= n) { return i; }
@@ -79,13 +83,14 @@ class CanvasImage {
                     }
                 }
                 for (j = ms, dstIt = i * bpr + j * 4 + c; j < w - ms; j++, dstIt += 4) {
-                    // Add rigtmost values to the histogram
-                    for (srcIt = offset + (j + ms) * 4, srcIt += bpr) {
+                    // Добавление крайних левых значений гистограммы
+                    for (srcIt = offset + (j + ms) * 4,
+                        l = srcIt + bpr * size; srcIt < l; srcIt += bpr) {
                         hist[d[srcIt]]++;
                     }
 
                     buff[dstIt] = getMiddle(hist, mc);
-                    // Remove leftmost values to the histogram
+                    //Удаление крайних левых значений гистограммы
                     for (srcIt = offset + (j - ms) * 4,
                         l = srcIt + bpr * size; srcIt < l; srcIt += bpr) { hist[d[srcIt]]--; }
                 }
@@ -107,34 +112,30 @@ class CanvasImage {
     }
 }
 class ImageFilter {
-    constructor(canvas, image_data) {
+    constructor(canvas, canvas_init, image_data, size_matrix) {
         this.canvas = canvas;
         this.image_data = image_data;
-
+        this.canvas_init = canvas_init;
+        this.size_matrix = size_matrix;
+    }
+    updateSizeMatrix(size_matrix) {
+        this.size_matrix = size_matrix;
     }
     start() {
         console.log("start", typeof this.image_data);
         let ctx = this.canvas.getContext("2d"), canvas = this.canvas;
-        // var startMedian = this.startMedian;
-        // let image = new Image();
-        // image.onload = function () {
-        //     console.log(image.data);
-        //     var radio = canvas.width / image.height
-        //     ctx.drawImage(image, 0, 0, canvas.width, canvas.height / radio);
-        //     startMedian(canvas);
-        // };
         var transformador = new CanvasImage(
             this.canvas,
-            this.image_data
+            this.canvas_init,
+            this.image_data,
+            this.size_matrix
         );
         setTimeout(() => {
             transformador.transform(function (r, g, b, a, factor, i) {
-                // здесь ваша магия...
                 return [255 - r, 255 - g, 255 - b, 255];
             });
-        }, 1000);
+        }, 200);
 
-        // image.src = this.image_data
     }
     startMedian(canvas) {
         console.log("toDataURL", canvas.toDataURL());
